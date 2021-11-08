4e0c44b924cec6d9487cacda2c94d3138d1d1b23
Victor Rodrigues <victorrodrigues@MacBook-Pro-de-Victor.local>
2021-11-08 13:12:43 -0300

    Adding initial project

---
+++ Sicredi/Sicredi/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Sicredi
+//
+//  Created by Victor Rodrigues on 08/11/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+}
+
---
+++ Sicredi/Sicredi/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Sicredi
+//
+//  Created by Victor Rodrigues on 08/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
